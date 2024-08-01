d8c862719b3311d1ecfed77e4b7441ac170dba8c
Jeevan <gairejeevan7@gmail.com>
2024-08-01 15:10:39 +0530

    passing children

--- 1st react app/react-app/src/App.tsx
+++ 1st react app/react-app/src/App.tsx
@@ -1,18 +1,24 @@
-import ListGroup from './components/ListGroup';
+// import ListGroup from './components/ListGroup';
+import Alert from './components/Alert';
 
 function App() {
-  let items = ['New York', 'San Francisco', 'London', 'Kathmandu', 'Beiging'];
-  const handleSelectedItem = (item: string) => {
-    console.log(item);
-  };
+  // let items = ['New York', 'San Francisco', 'London', 'Kathmandu', 'Beiging'];
+  // const handleSelectedItem = (item: string) => {
+  //   console.log(item);
+  
 
   return (
+    // <div>
+    //   <ListGroup
+    //     items={items}
+    //     heading="Cities"
+    //     onSelectItem={handleSelectedItem}
+    //   />
+    // </div>
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectedItem}
-      />
+      <Alert>
+        <span>Hello World!</span>
+        </Alert>
     </div>
   );
 }
