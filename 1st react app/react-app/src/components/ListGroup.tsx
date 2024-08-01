19cbd2a2ea92ebea36c93fb1a87de7e02af0c9f5
Jeevan <gairejeevan7@gmail.com>
2024-08-01 07:35:54 +0530

    handling events

--- 1st react app/react-app/src/components/ListGroup.tsx
+++ 1st react app/react-app/src/components/ListGroup.tsx
@@ -1,19 +1,21 @@
 // import { Fragment } from "react/jsx-runtime";
+import { MouseEvent } from 'react';
 
 function ListGroup() {
-  let items = ["New York", "San Francisco", "London", "Kathmandu", "Beiging"];
-  items = []
-
-  
+  let items = ['New York', 'San Francisco', 'London', 'Kathmandu', 'Beiging'];
+  // items = []
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
       <h1>List</h1>
       {items.length === 0 ? <p>No item found</p> : null}
       {items.length === 0 && <p>No item found</p>}
-      <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+      <ul className="list-group" onClick={handleClick}>
+        {items.map(item => (
+          <li className="list-group-item" key={item}>
+            {item}
+          </li>
         ))}
       </ul>
     </>
