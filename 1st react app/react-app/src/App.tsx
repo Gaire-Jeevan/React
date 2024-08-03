cb45cad52faf5d3d488c52a1d50651ee8ffb28a3
Jeevan <gairejeevan7@gmail.com>
2024-08-03 07:36:26 +0530

    simplifying update logic with immer

--- 1st react app/react-app/src/App.tsx
+++ 1st react app/react-app/src/App.tsx
@@ -19,6 +19,7 @@ function App() {
 
   return <div>
     {bugs.map(bug => <p key={bug.id}>{bug.title} {bug.fixed ? 'Fixed' : 'New'}</p>)}
+    <button onClick={handleClick}>Click Me</button>
   </div>;
 }
 
