d2e044977cd69eacaa013afc525f99ed50db273a
Jeevan <gairejeevan7@gmail.com>
2024-08-03 15:20:48 +0530

    accessing element of input field

--- 1st react app/react-app/src/App.tsx
+++ 1st react app/react-app/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import ExpandableText from './components/ExpandableText';
+import Form from './components/Form';
 
 
 function App() {
@@ -11,9 +12,7 @@ function App() {
 
   return (
     <div>
-      <ExpandableText maxChars={10}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam culpa ea hic officia, quisquam maxime quibusdam nemo pariatur aliquid quam quaerat impedit magnam vitae eos dolorem exercitationem cumque atque sit voluptates. Maxime, reprehenderit amet pariatur sint corrupti nostrum repellat possimus voluptatem magni magnam quibusdam et aperiam totam ipsum nihil accusamus temporibus distinctio, natus optio ex quasi obcaecati? Sapiente reprehenderit debitis ullam facere quam voluptatem beatae nihil mollitia sit consectetur iusto, minus molestias hic fugit esse veniam provident facilis. Veniam commodi ratione veritatis aperiam? Nemo officiis nisi, illo nostrum, iusto dicta esse error distinctio nulla aliquam, in praesentium. Magni, corporis obcaecati.
-      </ExpandableText>
+      <Form></Form>
     </div>
   );
 }
